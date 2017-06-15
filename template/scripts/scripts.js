8a98145c8e9197c051bdedd4855b1352c680cf64
e5ash <e5ash.bro@gmail.com>
2017-06-15 23:00:54 +0300

    Final

--- template/scripts/scripts.js
+++ template/scripts/scripts.js
@@ -2,7 +2,27 @@ $(document).ready(function($) {
 	$('.slider__list').slick({
 		slidesToShow: 5,
 		prevArrow: '<button type="button" class="slick-prev slick-arrow"><i class="icon icon-arrow-left"></i></button>',
-		nextArrow: '<button type="button" class="slick-next slick-arrow"><i class="icon icon-arrow-right"></i></button>'
+		nextArrow: '<button type="button" class="slick-next slick-arrow"><i class="icon icon-arrow-right"></i></button>',
+		responsive: [{
+
+	      breakpoint: 1311,
+	      settings: {
+	        slidesToShow: 3,
+	        infinite: true
+	      }}, {
+	      breakpoint: 671,
+	      settings: {
+	        slidesToShow: 2,
+	        infinite: true
+	      }}, {
+	      breakpoint: 501,
+	      settings: {
+	        slidesToShow: 1,
+	        infinite: true
+	      }
+
+    }]
+
 	});
 
 	var navHidden = $('.nav__hidden'),
@@ -21,5 +41,25 @@ $(document).ready(function($) {
 	}, function() {
 		navHidden.toggleClass('nav__hidden_toggle');
 		navLink.removeClass('nav__item_hover');
+	});
+	
+
+
+	var navBtn = $('.nav__btn'),
+		navList = $('.nav .container > ul');
+
+	navBtn.click(function() {
+		$(this).toggleClass('nav__btn_toggle');
+		navList.slideToggle(0);
+	});
+
+	var w = $(window);
+
+	w.on('resize', function() {
+		var windowWidth = $(this).width();
+		if (windowWidth > 1085) {
+			navList.removeAttr('style');
+			navBtn.removeClass('nav__btn_toggle');
+		}
 	});
 });
